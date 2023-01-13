b6a13b0022035c913d440a18746d0290d2a50f13
kyongsikC <kyeongsik.choi@greenlabs.co.kr>
2023-01-14 00:19:51 +0900

    logger middleware

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
+import { LoggerMiddleware } from 'src/common/middleware/logger.middleware';
 
 @Module({
   controllers: [CatsController],
-  providers: [CatsService]
+  providers: [CatsService],
 })
-export class CatsModule {}
+export class CatsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'cats', method: RequestMethod.ALL });
+  }
+}
